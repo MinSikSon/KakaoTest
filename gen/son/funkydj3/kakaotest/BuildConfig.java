4a48bea7ea8d2d5d27db7b5eb00460018db652bb
MinSikSon <tofu89223@gmail.net>
2015-04-23 02:38:49 +0900

    init first

---
+++ gen/son/funkydj3/kakaotest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package son.funkydj3.kakaotest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/son/funkydj3/kakaotest/MainActivity.java
@@ -0,0 +1,78 @@
+package son.funkydj3.kakaotest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.kakao.KakaoLink;
+import com.kakao.KakaoParameterException;
+import com.kakao.KakaoTalkLinkMessageBuilder;
+
+
+public class MainActivity extends Activity {
+	private KakaoLink kakaoLink;
+	private KakaoTalkLinkMessageBuilder kakaoTalkMessageBuilder;
+	private EditText mEditText;
+	private Button mSendBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    	Log.d("SON","this");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        try {
+			kakaoLink = KakaoLink.getKakaoLink(getApplicationContext());
+			kakaoTalkMessageBuilder = kakaoLink.createKakaoTalkLinkMessageBuilder();
+		} catch (KakaoParameterException e) {
+			e.printStackTrace();
+		}
+        
+        mEditText = (EditText)findViewById(R.id.editText);
+        mSendBtn = (Button)findViewById(R.id.btnSend);
+        mSendBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				sendLink();
+			}
+		});
+        
+    }
+    
+    private void sendLink(){
+    	try {
+			kakaoTalkMessageBuilder.addText(mEditText.getText().toString());
+			final String linkContents = kakaoTalkMessageBuilder.build();
+			kakaoLink.sendMessage(linkContents, this);
+		} catch (KakaoParameterException e) {
+			e.printStackTrace();
+		}
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
